fb20e48d61d0fec330a7570388c2156270ddbb6d
mig29-62 <12584@holbertonstudents.com>
2025-11-24 19:12:49 +0000

    holberton

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-words = str.split()
-str = words[5]+' '+words[6]+' '+words[12]+' '+words[0]
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str=str.split()
+print(str[5]+' '+str[6]+' '+str[12]+' '+str[0])
