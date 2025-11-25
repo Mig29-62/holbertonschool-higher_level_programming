27bf3291fdb06dd698333bd115226cf8aa25701b
mig29-62 <12584@holbertonstudents.com>
2025-11-25 13:03:01 +0000

    holberton

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,8 +2,8 @@
 i = 97
 str = ''
 while i < 123:
-           str = str + chr(i) + ''
-           i=i+1
-str=str.replace('e','')
-str=str.replace('q','')
+           str += chr(i)
+           i = i + 1
+str = str.replace('e', '')
+str = str.replace('q', '')
 print("{}".format(str), end='')
