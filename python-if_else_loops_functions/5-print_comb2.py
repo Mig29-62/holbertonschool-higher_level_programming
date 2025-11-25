3701a84abc64b75a8f58f81c3cb408561394a599
mig29-62 <12584@holbertonstudents.com>
2025-11-25 18:35:47 +0000

    holberton

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+i = 0
+j = 0
+string=''
+while i < 10:
+        j = 0
+        while j < 10:
+            print(f"{i}{j}",end=', ')
+            j +=  1
+        i += 1
+print('\n')
