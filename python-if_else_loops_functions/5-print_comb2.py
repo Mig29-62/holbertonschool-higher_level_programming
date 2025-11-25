192e7c1dcee1bedbffaa7b977e325b283766b82f
mig29-62 <12584@holbertonstudents.com>
2025-11-25 19:09:41 +0000

    holberton

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,8 +3,8 @@ i = 0
 j = 0
 string = ''
 for n in range(0, 99):
-    i = n//10
-    j = n%10
+    i = n // 10
+    j = n % 10
     str = "{}{},".format(i,j)
-    print(str,end = ' ')
+    print(str, end = ' ')
 print('99',end = '\n')
