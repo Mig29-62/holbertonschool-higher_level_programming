a5ef2fdfc0e2fca6c76696b7a77ae201691b5bc8
mig29-62 <12584@holbertonstudents.com>
2025-11-25 19:15:31 +0000

    holberton

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -5,6 +5,6 @@ string = ''
 for n in range(0, 99):
     i = n // 10   # noqa: E231,E251
     j = n % 10   # noqa: E231,E251
-    str = "{}{}".format(i,j)   # noqa: E251  #noqa: E231
+    str = "{}{}".format(i, j)   # noqa: E251  #noqa: E231
     print(str, end=', ')   # noqa: E251,E231
 print('99', end='\n')
