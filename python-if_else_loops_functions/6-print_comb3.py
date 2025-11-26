2d8412ee260bd8017abbc1a9315e7eb32087740d
mig29-62 <12584@holbertonstudents.com>
2025-11-26 13:04:59 +0400

    holberton

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,11 +5,11 @@ string = ''
 for n in range(0, 89):
     i = n // 10   # noqa: E231,E251
     j = n % 10   # noqa: E231,E251
-    if i < j :
+    if i < j:
         str = "{}{}".format(i, j)  # noqa: E251  #noqa: E231
         print(str, end=', ')  # noqa: E251,E231
     elif i == j:
         continue
     else:
         continue
-print('89', end='\n')
+print('89', end='\n')  #noqa: W292
