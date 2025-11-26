d409296825cff1751400c5011bd42d98e72370b6
mig29-62 <12584@holbertonstudents.com>
2025-11-26 13:50:13 +0400

    holberton

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string2=''
+    length=len(str)
+    for i in range(0,len):
+        c=ord(str[i])
+        c=c-32
+        string2=string2+chr(c)
+    print(string2)
