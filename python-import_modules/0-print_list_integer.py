8c7411198840037cbdb2af581acf0d65e446d4a1
mig29-62 <12584@holbertonstudents.com>
2025-11-27 20:48:39 +0000

    holberton

---
+++ python-import_modules/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    length = len(my_list)
+    for i in range(0,length)
+        value=my_list[i]
+        string="{}".format(value)
+        print(string)
