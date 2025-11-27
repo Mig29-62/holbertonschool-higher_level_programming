87422c13e1a5bf9718dd75a1e874000e3d687420
mig29-62 <12584@holbertonstudents.com>
2025-11-27 13:49:49 +0000

    holberton

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-	pass
+    pass
 
 a = 1
 b = 2
-c = add(a,b)
-string="{} + {} = {}".format(a,b,c)
+c = add(a, b)
+string = "{} + {} = {}".format(a, b , c)
 print(string)
