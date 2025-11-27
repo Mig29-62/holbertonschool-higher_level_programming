5626c47e90ff80331977147287e3d1c347e1aaf4
mig29-62 <12584@holbertonstudents.com>
2025-11-27 14:19:18 +0000

    holberton

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,17 +2,17 @@
 from calculator_1 import add , sub ,mul, div
 a = 10
 b = 5
-c = add(a,b)
-d = sub(a,b)
-e = mul(a,b)
-f = div(a,b)
-string="{} + {} = {}".format(a,b,c)
+c = add(a, b)
+d = sub(a, b)
+e = mul(a, b)
+f = div(a, b)
+string = "{} + {} = {}".format(a, b, c)
 print(string)
-string="{} - {} = {}".format(a,b,d)
+string = "{} - {} = {}".format(a, b, d)
 print(string)
-string="{} * {} = {}".format(a,b,e)
+string = "{} * {} = {}".format(a, b, e)
 print(string)
-string="{} / {} = {}".format(a,b,f)
+string = "{} / {} = {}".format(a, b, f)
 print(string)
 if  __name__ == "__main__":
     pass
