5d64090da6bbf50bcea28e800640ea7b6cbd0266
mig29-62 <12584@holbertonstudents.com>
2025-11-28 16:58:39 +0400

    holberton

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string=''
+    new_string = ''
     for ch in my_string:
-        if ch not in('c','C'):
+        if ch not in ('c', 'C'):
             new_string += ch
     return new_string
