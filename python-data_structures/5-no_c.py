56731600263d81c7c209fcf03b8a7079db360f67
mig29-62 <12584@holbertonstudents.com>
2025-11-28 16:27:12 +0400

    holberton

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     length = len(my_string)
     for i in range(0,length-1):
-        if ord(my_string[i]) != 99:
+        if ord(my_string[i]) == 99:
             new_string=my_string[:i]+my_string[i+1:]
             return new_string
-        if ord(my_string[i]) != 67:
+        if ord(my_string[i]) == 67:
             new_string = my_string[:i] + my_string[i + 1:]
