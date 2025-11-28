237c98d67c258fd99efbd59e8aa651269999ebdb
mig29-62 <12584@holbertonstudents.com>
2025-11-28 15:44:47 +0400

    holberton

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string -= str(chr(99))
-    my_string -= str(chr(67))
-    print(string)
+    my_string=my_string.strip('c')
+    my_string = my_string.strip('C')
+    print(my_string)
