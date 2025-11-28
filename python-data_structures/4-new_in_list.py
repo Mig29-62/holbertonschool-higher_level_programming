d20aa64bb7a79c159963c7eea2387b464622ea43
mig29-62 <12584@holbertonstudents.com>
2025-11-28 15:13:40 +0400

    holberton

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     length = len(my_list)
-    new_list[length]
     new_list = my_list
+    new_list[length]
     if idx < 0:
         return my_list
     elif idx > length-1:
