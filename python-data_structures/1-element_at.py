af1f0ddeaafa581959c309d8f85bb8490d0f446b
mig29-62 <12584@holbertonstudents.com>
2025-11-28 14:35:56 +0400

    holberton

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from threading import main_thread
+
+
+def element_at(my_list,idx):
+    l=len(my_list)
+    if idx < 0:
+        return None
+    elif idx > l:
+        return None
+    else:
+        return my_list[idx]
