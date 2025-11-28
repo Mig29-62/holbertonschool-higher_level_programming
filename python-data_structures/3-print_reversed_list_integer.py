e2c63b35c23f1ef0548a1033e475d74906f072c1
mig29-62 <12584@holbertonstudents.com>
2025-11-28 14:47:10 +0400

    holberton

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(mylist=[]):  # noqa: E251
+    length = len(mylist)
+    for i in range(length,0):
+        string = "{:d}".format(mylist[i])
+        print(string, end='\n')  # noqa: E251
