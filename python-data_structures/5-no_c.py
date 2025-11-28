2b092483a5733a87256c66456ea9fbabccdd56ec
mig29-62 <12584@holbertonstudents.com>
2025-11-28 16:36:48 +0400

    holberton

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,9 +3,9 @@ def no_c(my_string):
     length = len(my_string)
     for i in range(0,length-1):
         if ord(my_string[i]) == 67:
-            new_string = my_string[:i] + my_string[i + 1:]
+            new_string = my_string.strip("c")
         elif ord(my_string[i]) == 99:
-            new_string=my_string[:i]+my_string[i+1:]
+            new_string = my_string.strip("C")
 
 
     return new_string
