00019256c1670e02bfc22a9610362201da0bb00b
mig29-62 <12584@holbertonstudents.com>
2025-11-28 14:51:08 +0400

    holberton

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(mylist=[]):  # noqa: E251
     length = len(mylist)
-    for i in range(length,0):
-        print(i)
+    i = 0
+    while i < length:
         string = "{:d}".format(mylist[i])
         print(string, end='\n')  # noqa: E251
+        i += 1
