c97509f2bd3f82ee90d5c546c53d119e611fa912
mig29-62 <12584@holbertonstudents.com>
2025-11-28 14:37:56 +0400

    holberton

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list,idx):
     l=len(my_list)
     if idx < 0:
         return None
-    elif idx > l:
+    elif idx > l-1:
         return None
     else:
         return my_list[idx]
