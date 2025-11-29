4880a01254f6f27ccd3a385e4d584a9a9b4da90d
mig29-62 <12584@holbertonstudents.com>
2025-11-29 20:16:31 +0400

    holberton

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_c=[]
     for i in range(0,2):
-        if tuple_a[i] or tuple_b[i]:
+        if tuple_a[i] or tuple_b[i] in tuple_a or tuple_b:
             c=tuple_a[i]+tuple_b[i]
             list_c.append(c)
         else:
