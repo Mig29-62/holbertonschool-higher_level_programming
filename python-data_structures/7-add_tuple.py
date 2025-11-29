aad38d51aa61e751d27eb30bfb870d9a02a7bae4
mig29-62 <12584@holbertonstudents.com>
2025-11-29 20:23:34 +0400

    holberton

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     list_c=[]
-    list_c=list(tuple_a) + list(tuple_b)
+    list_c=[x+y for x,y in zip(tuple_a, tuple_b)]
     tuple_c=tuple(list_c)
     return tuple_c
