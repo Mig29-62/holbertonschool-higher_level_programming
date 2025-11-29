3002bb856cc5d3eba7fb21b54ca07fdd306fd9e7
mig29-62 <12584@holbertonstudents.com>
2025-11-29 12:43:21 +0000

    holberton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -14,7 +14,7 @@ def print_matrix_integer(a=[[]]):
                     print(string,end=' ')
                 j += 1
             if i == row-1:
-               pass
+               print('')
             else:
                print('\n')
             i += 1
