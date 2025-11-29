aca5553437fdc42f47ece187995e63afe7838d38
mig29-62 <12584@holbertonstudents.com>
2025-11-30 00:45:22 +0400

    holbertonn

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_matrix = list(my_list)
-    while search in my_list:
-            search_index = my_list.index(search)
-            new_matrix[search_index] = replace
+    new_matrix = [s.replace(search, replace) for s in my_list]
     return new_matrix
 
