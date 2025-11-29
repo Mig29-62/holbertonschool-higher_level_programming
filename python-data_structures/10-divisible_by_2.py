86766408669f07db5468960720f8db5ef3bb272a
mig29-62 <12584@holbertonstudents.com>
2025-11-29 22:36:02 +0400

    holbertonn

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    list_value=[]
+    list_value = []
     for element in my_list:
         if element % 2 == 0:
             list_value.append(True)
