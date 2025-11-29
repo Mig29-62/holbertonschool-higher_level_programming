18088210fc498ea779a2605d9e38496f1d4b8e21
mig29-62 <12584@holbertonstudents.com>
2025-11-30 00:12:48 +0400

    holberton

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    length = len(matrix)
-    length_2=len(matrix[0])
+    new_matrix = matrix
+    length = len(new_matrix)
+    length_2=len(new_matrix[0])
     for i in range(length):
         for j in range(length_2):
-            matrix[i][j]=matrix[i][j]**2
-    return matrix
+            new_matrix[i][j]=new_matrix[i][j]**2
+    return new_matrix
