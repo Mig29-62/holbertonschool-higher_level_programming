4afa2c9b329a3c1046aa923fc873932cd15942f2
mig29-62 <12584@holbertonstudents.com>
2025-11-29 22:26:54 +0400

    holbertonn

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     length=len(my_list)
-    i=0
-    j=0
     max=0
-    while i < length-1:
-        while j < length-1:
-            if my_list[i]>my_list[j]:
-                max=my_list[i]
-            else:
-                max=my_list[j]
-            j=j+1
-        i=i+1
+    for element in my_list:
+        if element > max:
+            max=element
+        else:
+            pass
     return max
