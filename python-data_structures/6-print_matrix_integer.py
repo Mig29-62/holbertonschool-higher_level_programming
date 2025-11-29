1a4e67a65160d84fcf2dbd420205580414cdf5e2
mig29-62 <12584@holbertonstudents.com>
2025-11-29 12:15:38 +0000

    holberton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     columns=len(matrix[0])
     i=0
     j=0
+    string=''
     while i<row:
             while j<column:
-                print(matrix[i][j],end=' ')
+                string=a[i][j]+'' 
+                print(string,end=' ')
                 j += 1
             print("\n")
             i += 1
