a0243e5631d6fa622e1cb0a50821c0b2d201b6e2
mig29-62 <12584@holbertonstudents.com>
2025-11-29 12:25:56 +0000

    holberton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,7 +8,10 @@ def print_matrix_integer(a=[[]]):
     while i<row:
             while j<column:
                 string="{:d}".format(a[i][j])
-                print(string,end=' ')
+                if j == column-1:
+                    print(string,end='')
+                else:
+                    print(string,end=' ')
                 j += 1
             print("\n")
             i += 1
