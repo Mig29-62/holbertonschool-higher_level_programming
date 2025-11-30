76b3fdf3a0be91ca08d6426288676c405a4c481b
mig29-62 <12584@holbertonstudents.com>
2025-11-30 15:35:32 +0400

    holbertonn

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,6 @@
+def uniq_add(my_list=[]):
+    new_list=list(set(my_list))
+    addition=0
+    for element in my_list:
+        if element in my_list and not new_list:
+            addition += element
