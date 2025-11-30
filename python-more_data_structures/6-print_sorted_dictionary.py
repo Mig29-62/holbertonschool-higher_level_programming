e0c3a5b45f7af466c58564b44a51e21c92a5a122
mig29-62 <12584@holbertonstudents.com>
2025-11-30 19:53:36 +0400

    holbertonn

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_sorted=dict(sorted(a_dictionary.items()))
-    for element in dict_sorted.items():
-        print (dict_sorted(element),end='\n')
+    l1=dict(sorted(a_dictionary.items()))
+    for element in l1():
+        print (l1(element), end='\n')
