ca929d25e3ebdfc7c3f8dc8d8c84d63d49bce858
mig29-62 <12584@holbertonstudents.com>
2025-11-30 20:11:27 +0400

    holbertonn

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     l1=list(sorted(a_dictionary.keys()))
-    l2=list(a_dictionary.values())
+    l2=dict(list(a_dictionary.values()))
     for key in l1:
             string='{}:{}'.format(key,l2[key])
             print(string,end='\n')
