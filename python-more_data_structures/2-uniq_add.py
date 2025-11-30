a02b3c76044e35a1471356a95a02526b752d2a54
mig29-62 <12584@holbertonstudents.com>
2025-11-30 15:54:45 +0400

    holbertonn

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,9 @@ def uniq_add(my_list=[]):
     new_list=list(set(my_list))
     addition=0
     for element in my_list:
-        if element in my_list and not new_list:
-            addition += element
+        count=my_list.count(element)
+        if count > 1:
+            addition=addition+element
         else:
             continue
     return addition
