041581cfdf28ee5e7ca96b6dd22c33be050d302a
mig29-62 <12584@holbertonstudents.com>
2025-11-30 16:35:57 +0400

    holbertonn

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    empty_set=set()
+    empty_set = set()
     for element in set_1:
         if element in set_2:
             empty_set.add(element)
