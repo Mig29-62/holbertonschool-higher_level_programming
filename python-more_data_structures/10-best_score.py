b39ea83d7bb292098a7348c220348e83d700991a
mig29-62 <12584@holbertonstudents.com>
2025-12-01 18:36:02 +0400

    holberton

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     maximum=0
     for key in a_dictionary:
         a=a_dictionary[key]
+        a=int(a)
         if a > maximum:
             maximum = key
     return maximum
