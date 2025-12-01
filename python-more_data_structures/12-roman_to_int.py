8840027c969928f43667d99a9b759d3fcbcbf476
mig29-62 <12584@holbertonstudents.com>
2025-12-01 20:18:39 +0400

    holberton

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,8 @@ def roman_to_int(roman_string):
         pos=roman_string.find(letter)
         if roman_string[pos+1] > roman_string[pos]:
             addition -= roman_dict[letter]
+        elif len(roman_string) == 1:
+            addition += roman_dict[letter]
         else:
             addition += roman_dict[letter]
     return addition
