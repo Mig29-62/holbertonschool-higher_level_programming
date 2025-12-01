d33500f85e457b1d9b3da28ec08643f9bb792eb9
mig29-62 <12584@holbertonstudents.com>
2025-12-01 13:34:46 +0400

    holbertonn

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    l1 = list(sorted(a_dictionary.keys()))
-    l2 = list(a_dictionary.values())
-    for key in l1:
-        for value in l2:
-            if a_dictionary[key] == value:
-                string = '{}: {}'.format(key, value)
-                print(string, end='\n')
-            else:
-                continue
+    for key in a_dictionary:
+        print("{}: {}".format(key, a_dictionary[key]))
