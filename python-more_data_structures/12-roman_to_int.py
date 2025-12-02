0c501db054daf0d9ea4cf87b0f3c7506e1e66c50
mig29-62 <12584@holbertonstudents.com>
2025-12-02 15:38:31 +0400

    holberton

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I' : 1,'V' : 5,'X' : 10,'L' : 50,'C' : 100,'D' : 500,'M' : 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
     addition = 0
     prev = 0
     if not type(roman_string) == str:
