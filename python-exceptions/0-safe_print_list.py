512676246ea77b9bd46ed34c63929e456301914f
mig29-62 <12584@holbertonstudents.com>
2025-12-03 00:32:44 +0400

    holberton

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    addition=0
+    for element in my_list:
+        addition += 1
     try:
         for i in range(0,x):
             print(my_list[i],end='')
         print('\n',end='')
         return x
-    except IndexError:
-        addition=0
+    except x != addition:
         for element in my_list:
             print(element,end='')
-            addition += 1
         print('\n',end='')
         return addition
