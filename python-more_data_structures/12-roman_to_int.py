de44e06c15bab2f3bd9221cfd6ac096048d6afc1
mig29-62 <12584@holbertonstudents.com>
2025-12-02 15:34:28 +0400

    holberton

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,6 @@ def roman_to_int(roman_string):
     roman_dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
     addition=0
     prev=0
-    length=len(roman_string)
     for element in reversed(roman_string):
         if roman_dict[element] < prev:
             addition -= roman_dict[element]
