664b48efec0e701a0b358e85a075be27070febc0
mig29-62 <12584@holbertonstudents.com>
2025-12-04 01:27:03 +0400

    holberton

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,11 +3,11 @@ def safe_print_division(a, b):
     try:
         c=a/b
     except ZeroDivisionError:
-
+        return None
     finally:
         print("Inside result: ", end='')
         if b != 0:
             print("{:.1f}".format(c))
         else:
             print(None)
-        return c
+    return c
