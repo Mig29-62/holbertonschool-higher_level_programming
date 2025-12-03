db8b2b72569196a8086962158991e22147b23969
mig29-62 <12584@holbertonstudents.com>
2025-12-04 01:05:34 +0400

    holberton

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         print("Inside result: ",end='')
-    except TypeError:
-        pass
+    except ZeroDivisionError:
+        return None
     finally:
         print("{:.1f}".format(a/b))
         return a/b
