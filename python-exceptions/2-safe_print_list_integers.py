f986a87ae535baedf6b7ae7f96ea28a11f891260
mig29-62 <12584@holbertonstudents.com>
2025-12-03 22:11:21 +0400

    holberton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     addition = 0
-    i = 0
     for element in my_list:
         if  isinstance(addition,int):
             addition += 1
