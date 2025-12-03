f0ca1f7c1bc5da520aecbdaafbde795a16a72292
mig29-62 <12584@holbertonstudents.com>
2025-12-03 22:36:28 +0400

    holberton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,18 @@
 def safe_print_list_integers(my_list=[], x=0):
     addition = 0
     for element in my_list:
-        if  isinstance(element,int):
+        if isinstance(element, int):
             addition += 1
         else:
             continue
-    for i in range(0,x):
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]),end='')
+            print("{:d}".format(my_list[i]), end='')
         except ValueError:
             continue
         except TypeError:
             continue
-    print('\n',end='')
+    print('\n', end='')
     if x < addition:
         return x
     elif x > addition:
