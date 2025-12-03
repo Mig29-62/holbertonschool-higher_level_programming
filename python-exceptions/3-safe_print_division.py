f590655cfeb4f4d3f39577ec7b0bb35212df6f02
mig29-62 <12584@holbertonstudents.com>
2025-12-04 01:04:17 +0400

    holberton

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = a/b
         print("Inside result: ",end='')
     except:
         pass
     finally:
-        print("{:.1f}".format(c))
-        return c
+        print("{:.1f}".format(a/b))
+        return a/b
