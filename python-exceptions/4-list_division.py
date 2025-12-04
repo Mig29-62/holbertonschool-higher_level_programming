9ba166821466c171fe7b8092df79df3f697b5937
mig29-62 <12584@holbertonstudents.com>
2025-12-04 14:56:35 +0400

    holberton

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,3 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             empty_list.append(0)
+        finally:
+            return empty_list
