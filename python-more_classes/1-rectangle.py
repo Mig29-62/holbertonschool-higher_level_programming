2ff33703dff5a1592ae2ea55f4611432285fe739
mig29-62 <12584@holbertonstudents.com>
2025-12-05 21:28:14 +0400

    holberton

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""we define a rectangle class here"""
+
+
+class Rectangle:
+    """a simple rectangle class"""
+
+def __init__(self,width=0,height=0):
+    self._width = width
+    self._height = height
+
+@property
+def width(self):
+    return self._width
+
+@width.setter
+def width(self,value):
+    if  not isinstance(value,int):
+        raise TypeError("width must be an integer")
+    elif value < 0:
+        raise ValueError("width must be >= 0")
+    else:
+        self._width = value
+
+@property
+def height(self):
+    return self._height
+
+@height.setter
+def height(self,value):
+    if not isinstance(value,int):
+        raise TypeError("height must be an integer")
+    elif value < 0:
+        raise ValueError("width must be >= 0")
+    else:
+        self._height = value
