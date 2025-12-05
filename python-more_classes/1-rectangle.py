379496efdd5f88ef68aec4bcffe8006c43722a59
mig29-62 <12584@holbertonstudents.com>
2025-12-05 21:31:44 +0400

    holberton

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """a simple rectangle class"""
 
-def _init_(self,width=0,height=0):
+def __init__(self, width=0, height=0):
     self._width = width
     self._height = height
 
