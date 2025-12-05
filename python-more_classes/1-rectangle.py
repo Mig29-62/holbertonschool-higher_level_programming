c8b774eab94846761d4736ce3392024f2ac8ce16
mig29-62 <12584@holbertonstudents.com>
2025-12-05 22:21:23 +0400

    holberton

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self,value):
@@ -20,11 +20,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self,value):
@@ -33,4 +33,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self._height = value
+            self.__height = value
