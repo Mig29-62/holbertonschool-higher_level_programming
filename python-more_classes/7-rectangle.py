e44c1c221ca231652fc5451e18de4bb56f83e01b
mig29-62 <12584@holbertonstudents.com>
2025-12-07 15:52:19 +0400

    holbertonnn

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """a simple rectangle class"""
 
     number_of_instances = 0
-    print_symbol='#'
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
