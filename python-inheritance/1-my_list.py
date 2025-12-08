58c232a9a27ee9ee0f7872e56c7ee1f06ca53f72
mig29-62 <12584@holbertonstudents.com>
2025-12-08 14:01:42 +0400

    holberton

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """module contains the class which prints list in a sorted order"""
 
 
-class Mylist(list):
+class MyList(list):
 
     """class contains function which defines print function using sorted function"""
 
