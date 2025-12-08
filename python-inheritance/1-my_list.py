0a6344ea7cdde9b00fb7ba8d36d5df27b78756bd
mig29-62 <12584@holbertonstudents.com>
2025-12-08 14:53:25 +0400

    holberton

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""module contains the class which prints list in a sorted order"""
-
+"""Module contains the class which prints a list in sorted order."""
 
 class MyList(list):
-
-    """class contains function which defines print function using sorted function"""
+    """Class that extends list and adds a method to print it sorted."""
 
     def print_sorted(self):
-        print([item for item in sorted(self)])
-
+        """Prints the list in ascending order."""
+        print(sorted(self))
