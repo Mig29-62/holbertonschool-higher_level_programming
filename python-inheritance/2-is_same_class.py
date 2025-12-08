baba2c51830241e29d7dea0ec48048f55df5685c
mig29-62 <12584@holbertonstudents.com>
2025-12-08 15:37:40 +0400

    holberton

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""in this module we define is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+
+    """in this function we use isinstance to detect if the object is an instance of class"""
+
+    if isinstance(obj,a_class):
+        return True
+    else:
+        return False
