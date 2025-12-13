88436db78159d1782977de20b57ad972991b1b24
mig29-62 <12584@holbertonstudents.com>
2025-12-13 23:17:53 +0400

    holbertonn

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""in this module we turn a python data to json"""
+
+
+def to_json_string(my_obj):
+
+    """we use json dumps method to turn string into json"""
+
+    return json.dumps(my_obj)
