e2ccc8c16620fd9d763456dc1898b98d6060ecf8
mig29-62 <12584@holbertonstudents.com>
2025-12-13 20:48:01 +0400

    holbertonn

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,5 +2,5 @@
 """we define file writing function in this module"""
 def write_file(filename='',text=''):
     """we write to file using write method"""
-    with open("filename",encoding='utf-8') as f:
+    with open('filename',encoding='utf-8') as f:
         f.write(text)
