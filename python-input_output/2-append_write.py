eb14019ac1096364036ce11905d627e6f1fe90af
mig29-62 <12584@holbertonstudents.com>
2025-12-13 22:46:49 +0400

    holbertonn

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,7 +8,7 @@ def append_write(filename="", text=""):
 
     if filename:
         with open(filename, 'a') as f:
-            f.write(text)
+            return f.write(text)
     else:
         with open(filename, 'w') as f:
-            f.write(text)
+            return f.write(text)
