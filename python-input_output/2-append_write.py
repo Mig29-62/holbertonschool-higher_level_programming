2e13571a658708cc59dac205aa921bd99c8b8941
mig29-62 <12584@holbertonstudents.com>
2025-12-13 22:45:03 +0400

    holbertonn

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+"""we define append function in this module"""
+
+
+def append_write(filename="", text=""):
+
+    """we use 'a' and 'w' regimes to write to file"""
+
+    if filename:
+        with open(filename, 'a') as f:
+            f.write(text)
+    else:
+        with open(filename, 'w') as f:
+            f.write(text)
