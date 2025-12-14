84644df86583434b83a223d84891093802fdf9d6
mig29-62 <12584@holbertonstudents.com>
2025-12-15 01:20:16 +0400

    holbertonn

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,4 @@
 """we define object to json"""
 
 def class_to_json(obj):
-    dictionary={}
-    for key,value in obj.__doc__.items():
-        dictionary[key]=value
-    return dictionary
+    print(obj.__doc__)
