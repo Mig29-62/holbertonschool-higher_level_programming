c20824af7b171f6ef589750c180f786c45be5cc9
mig29-62 <12584@holbertonstudents.com>
2025-12-14 23:01:48 +0400

    holbertonn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """we define module"""
-save=__import__('5-save_to_json_file').save_to_json_file
-load=__import__('6-load_from_json_file').load_from_json_file
 import sys
-l=[]
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+l = []
 try:
     list2=load('add_item.json')
     l.extend(list2)
-except:
+except FileNotFoundError:
     pass
 l.extend(sys.argv[1:])
-save(l,'add_item.json')
+save(l, 'add_item.json')
