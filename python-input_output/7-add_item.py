f62354a76c2390a3c4f7bfd0fba5af1aaa2c65f2
mig29-62 <12584@holbertonstudents.com>
2025-12-14 22:39:33 +0400

    holbertonn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,10 @@
 save=__import__('5-save_to_json_file').save_to_json_file
 load=__import__('6-load_from_json_file').load_from_json_file
 import sys
-l=[]
-l.append(sys.argv[1:])
+l=list(sys.argv[1:])
+try:
+    list2=load('add_item.json')
+    l.append(list2)
+except:
+    pass
 save(l,'add_item.json')
