cc99ee6e32cb7bdae0962895bb00eb18731cfabb
mig29-62 <12584@holbertonstudents.com>
2025-12-14 22:34:58 +0400

    holbertonn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,5 +3,6 @@
 save=__import__('5-save_to_json_file').save_to_json_file
 load=__import__('6-load_from_json_file').load_from_json_file
 import sys
+l=[]
 l.append(list(sys.argv[1:]))
 save(l,'add_item.json')
