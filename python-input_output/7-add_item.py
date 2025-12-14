ea6b1106058f9a851a71d6ad5df71eb8608027e1
mig29-62 <12584@holbertonstudents.com>
2025-12-14 22:23:50 +0400

    holbertonn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 save=__import__('5-save_to_json_file').save_to_json_file
 load=__import__('6-load_from_json_file').load_from_json_file
 import sys
-if 'add_item.json':
+try:
     l=list(load('add_item.json'))
-else:
+except FileNotFoundError:
     pass
 l.append(list(sys.argv[1:]))
 save(l,'add_item.json')
