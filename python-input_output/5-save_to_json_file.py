35f9dc917df79214af335fa962b28cf15619502b
mig29-62 <12584@holbertonstudents.com>
2025-12-14 17:02:16 +0400

    holbertonn

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 def save_to_json_file(my_obj,filename):
 
     """we use loads and open method"""
-
+    a = json.dump(my_obj)
     with open(filename, 'w') as f:
-        f.write(my_obj)
+        f.write(a)
