bf52d55be26cf6dacf23be4d0ce8a7702982988d
mig29-62 <12584@holbertonstudents.com>
2025-12-18 00:40:02 +0400

    holbertonn

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,7 +3,7 @@ import csv
 import json
 def convert_csv_to_json(filename):
     with open(filename,'r') as f:
-        data=csv.DictReader(f)
+        data=list(csv.DictReader(f))
     with open("data.json",'w') as f:
         new_data=json.dump(data,f)
         return new_data
