8b5eedc8daaae53698531e7270486d00cfb56467
mig29-62 <12584@holbertonstudents.com>
2025-12-18 14:27:41 +0400

    holberton

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import xml.etree.ElementTree as ET
+def serialize_to_xml(dictionary, filename):
+    for key,value in dictionary.items():
+        elem=ET.Element("data")
+        if isinstance(value,dict):
+            child=serialize_to_xml(key,value)
+            elem.append(child)
+        else:
+            child=ET.SubElement(elem,key)
+            child.text=str(value)
+    return elem
