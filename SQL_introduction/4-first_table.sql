16dd80d88f71f351ffe6fa036dd4388c1bff59ac
mig29-62 <12584@holbertonstudents.com>
2025-12-18 12:50:52 +0000

    holberton

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
 --we use create method to create a table and some data inside
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
-USE hbtn_test_db_4;
-CREATE TABLE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table;
+INSERT INTO first_table(
     id INT,
     name VARCHAR(256)
 );
