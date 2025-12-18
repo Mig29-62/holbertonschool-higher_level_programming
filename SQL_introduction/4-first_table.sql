c25c5180e173c8438940568b6e2f462dab4f01af
mig29-62 <12584@holbertonstudents.com>
2025-12-18 17:01:08 +0000

    holberton

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---we use create method to create a table and some data inside
+-- we use create method to create a table and some data inside
 CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
