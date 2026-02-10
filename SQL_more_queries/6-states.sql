d49b74b4edbec6079172608f98f6c1c28c20a3dd
IsaevThunder <12584@holbertonstudents.com>
2026-02-10 10:51:25 -0500

    holberton

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- we use CREATE TABLE to create the table and also add elements.DEFAULT is used to put default id value as a condition by holberton task rules.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
 	id INT DEFAULT 1 UNIQUE PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
