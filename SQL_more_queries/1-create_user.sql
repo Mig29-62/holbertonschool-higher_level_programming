fcf055acebdab17d916b083c63dace9bda2f1181
IsaevThunder <12584@holbertonstudents.com>
2026-02-10 09:53:39 -0500

    holberton

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- we use CREATE command to create user and GRANT to grant priveleges to certain user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY  'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON '.' TO 'usexr_0d_1'@'localhost'; 
+GRANT ALL PRIVILEGES ON *.* TO 'usexr_0d_1'@'localhost'; 
 FLUSH PRIVILEGES;
