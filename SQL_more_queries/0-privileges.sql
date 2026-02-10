5168bfa1780b029974f2fc06dd23f1b5a9cc2a35
IsaevThunder <12584@holbertonstudents.com>
2026-02-10 09:37:27 -0500

    holberton

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- we use show command to see priveleges granted to user0d_1 and user0d_2
-SHOW GRANTS FOR 'user0d_1'@'localhost';
-SHOW GRANTS FOR 'user0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
