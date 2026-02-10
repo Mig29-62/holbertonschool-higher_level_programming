a7f8ff25b9317e75e614a09c0aedb92f29d68401
IsaevThunder <12584@holbertonstudents.com>
2026-02-10 10:29:35 -0500

    holberton

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
 -- we use CREATE TABLE to create the table and also add elements.COMMENT is used to add comment specified by per Holberton task rules.
-USE hbtn_test_db_3;
 CREATE TABLE IF NOT EXISTS force_name(
 	id INT,
 	name VARCHAR(256) NOT NULL,
