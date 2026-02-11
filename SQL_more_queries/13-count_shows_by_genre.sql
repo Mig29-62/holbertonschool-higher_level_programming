abe2cc39b046144090ea6a3dfc0ef1823ae47cd7
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 14:41:35 -0500

    holberton

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- we use where command inside a where command to take information out of other tables
 SELECT tv_genres.name,COUNT(tv_show_genres.show_id) AS occurence 
 FROM tv_genres,tv_shows
-LEFT JOIN tv_show_genres
-ON  tv_shows.id=tv_show_genres.show_id
+JOIN tv_show_genres
+ON  tv_genres.id=tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY  occurence DESC;
