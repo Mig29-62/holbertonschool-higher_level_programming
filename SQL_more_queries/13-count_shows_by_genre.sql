d4fbc2362b11bf850a0468ea0f16e3b7c5038e9b
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 14:24:10 -0500

    holberton

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,5 @@ FROM tv_genres,tv_shows
 LEFT JOIN tv_show_genres
 ON  tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_genres.name
 ORDER BY  occurence DESC;
