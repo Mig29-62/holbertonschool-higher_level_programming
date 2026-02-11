419a4eb0ee6154df742ff31a15e6227ae21d9d0b
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 15:35:40 -0500

    holberton

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,7 @@
--- WE USE SELECT WITH DISTINCT TO OVERCOME REPEATING SAME GENRE NAME AND WRITE OUT A CONDITION AS PER HOLBERTON TASK RULES.
-SELECT tv_shows.title,tv_genres.name
-FROM tv_genres,tv_shows
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id;
-ORDER BY tv_shows.title ASC,tv_genres.name ASC;
+-- WE USE TWO LEFT JOINS TO LINK ALL TABLES TOGETHER FROM AN ID VIEWPOINT AND ALSO SHOW MOVIES WITH NO GENRES 
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+
