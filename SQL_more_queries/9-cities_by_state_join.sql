079fabd4f10ed910edc8baa9c166b515393193de
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 11:51:25 -0500

    holberton

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- we use where command inside a where command to take information out of other tables
-SELECT id, name 
-FROM cities 
-JOIN states
-ON cities.state_id=states.id
+SELECT cities.id, cities.name,states.name 
+FROM cities,states 
+WHERE cities.state_id=states.id
 ORDER BY id ASC
