890f9cb97ed962a9903b10d014878125a5cd126f
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 15:00:52 -0500

    holberton

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- WE USE SELECT WITH DISTINCT TO OVERCOME REPEATING SAME GENRE NAME AND WRITE OUT A CONDITION AS PER HOLBERTON TASK RULES.
+SELECT DISTINCT tv_genres.name as name
+FROM tv_genres,tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id=tv_shows.id
+WHERE tv_shows.title='Dexter'
+ORDER BY tv_genres.name ASC; 
