c7f4eb82fe265d1d387485d3b330f8fe99434e1f
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 12:59:54 -0500

    holberton

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- we use where command inside a where command to take information out of other tables
+SELECT tv_shows.title,tv_show_genres.genre_id
+FROM tv_shows,tv_show_genres
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title ASC,tv_show_genres.genre_id ASC
