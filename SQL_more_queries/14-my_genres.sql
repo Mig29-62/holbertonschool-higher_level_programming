e5ced5d8e8266af4e8ad8ad53a83993202ba1889
IsaevThunder <12584@holbertonstudents.com>
2026-02-11 15:01:28 -0500

    holberton

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- WE USE SELECT WITH DISTINCT TO OVERCOME REPEATING SAME GENRE NAME AND WRITE OUT A CONDITION AS PER HOLBERTON TASK RULES.
-SELECT DISTINCT tv_genres.name as name
+SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres,tv_shows
 JOIN tv_show_genres
 ON tv_show_genres.show_id=tv_shows.id
