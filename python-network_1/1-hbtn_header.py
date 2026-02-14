04c9ea69666ca69ec7264d45b790c9332cfb850d
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 18:24:34 -0500

    holberton

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,8 +2,8 @@
 """we use urllib to get request from specific links"""
 import urllib.request
 import sys
-if __name__="__main__":
-    url =sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         print(response.info().get('X-Request-Id'))
