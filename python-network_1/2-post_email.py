57ca0a2ff806e4022e795afbe7e905884fc5d948
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 18:44:21 -0500

    holberton

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,9 +4,10 @@ import urllib.request
 import sys
 if __name__ == "__main__":
     url = sys.argv[1]
-    email="isayevbooks360@gmail.com"
+    email sys.argv[2]
     email=email.encode('utf-8')
     data={'email':email}
+    data=urllib.parse.urlencode(data).encode(utf-8)
     req = urllib.request.Request(url,data)
     with urllib.request.urlopen(req) as response:
         print(response.read())
