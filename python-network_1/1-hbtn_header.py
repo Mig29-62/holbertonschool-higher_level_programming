1b64a767a839e6d01770a63ba700a901d127f526
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 18:25:40 -0500

    holberton

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,5 +6,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        print(response.info().get('X-Request-Id'))
- 
+        print(response.info().get('X-Request-Id')) 
