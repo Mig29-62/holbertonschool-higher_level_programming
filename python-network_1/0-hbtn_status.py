0391eac4b26e8ce3fb667f5e38e6a21c5b78ae2f
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 17:41:49 -0500

    holberton

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """we use urllib to get request from specific links"""
 import urllib.request
 url = "https://intranet.hbtn.io/status"
-req=urllib.request.Request(url)
+req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as response:
-        body = response.read()
-        content = body.decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(content))
+    body = response.read()
+    content = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(content))
