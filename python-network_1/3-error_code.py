a1be621168f0c700325d3a8253e6baa62a7c34a0
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 20:12:40 -0500

    holberton

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -2,13 +2,12 @@
 """we use  try and except to manage errors,also using .code method to  print status code"""
 import urllib.request
 import sys
-if __name__="__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        req=urllib.request.Request()
-        with urrlib.request.openurl() as response:
+        with urllib.request.urlopen(url) as response:
             content=response.read()
             content=content.decode('utf-8')
             print(content)
-     except urllib.error.HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
