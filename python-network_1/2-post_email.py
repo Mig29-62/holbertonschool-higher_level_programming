f2f4a4f698368cd97ae2e3fa6dca659ca566b76d
IsaevThunder <12584@holbertonstudents.com>
2026-02-14 19:00:49 -0500

    holberton

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,11 +5,11 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    email=email.encode('utf-8')
-    data={'email':email}
-    data=urllib.parse.urlencode(data).encode('utf-8')
-    req = urllib.request.Request(url,data)
+    email = email.encode('utf-8')
+    data = {'email' : email}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(url , data)
     with urllib.request.urlopen(req) as response:
-        open=response.read()
-        open=open.decode('utf-8')
+        open = response.read()
+        open = open.decode('utf-8')
         print(open)
