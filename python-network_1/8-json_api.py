bc6959f6db32c9a6162bc08cff5e2d8178ead834
IsaevThunder <12584@holbertonstudents.com>
2026-03-24 18:32:45 -0400

    holberton

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -4,35 +4,19 @@ Sends a POST request to a search API with a letter as a parameter.
 """
 import requests
 import sys
-
-
 if __name__ == "__main__":
-    # URL to send the request to
     url = "http://0.0.0.0:5000/search_user"
-
-    # Set q to the first argument if it exists, otherwise use an empty string
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
-
-    # Prepare the payload
     payload = {'q': q}
-
     try:
-        # Send the POST request
         response = requests.post(url, data=payload)
-        
-        # Parse the JSON response
         user_data = response.json()
-
-        # Check if the dictionary is empty
         if not user_data:
             print("No result")
         else:
-            # Display the id and name in the format [<id>] <name>
             print("[{}] {}".format(user_data.get('id'), user_data.get('name')))
-
     except ValueError:
-        # This handles cases where response.json() fails
         print("Not a valid JSON")
