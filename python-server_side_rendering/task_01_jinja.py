581b846f1784c109464a57d226ebe56299a6cd5f
IsaevThunder <12584@holbertonstudents.com>
2026-03-26 09:06:28 -0400

    holberton

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -14,13 +14,6 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
-@app.route('/items')
-def items_list():
-    with open('items.json', 'r') as f:
-        data = json.load(f)
-    all_items = data.get("items", [])
-    return render_template('items.html', items=all_items)
-
+    return render_template('contact.html') 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
